9aee29b11b56d05c1fba360fa7aeb1ffe1eb68b9
Rohin Madhavan <rohinmadhavan96@gmail.com>
2024-10-06 19:33:01 +0100

    Initial Commit

---
+++ Fit Track/Fit_TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fit_TrackApp.swift
+//  Fit Track
+//
+//  Created by Rohin Madhavan on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Fit_TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fit TrackTests/Fit_TrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fit_TrackTests.swift
+//  Fit TrackTests
+//
+//  Created by Rohin Madhavan on 06/10/2024.
+//
+
+import Testing
+@testable import Fit_Track
+
+struct Fit_TrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
